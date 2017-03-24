a878b0c8e104118afd7d51a2bfe02030e3b782e2
Ben <benschenkenberger>
2017-03-24 12:04:19 -0700

    pass spec4: team assigned an id correctly

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -4,11 +4,13 @@ import java.util.List;
 public class Team {
   private String mName;
   private static List<Team> instances = new ArrayList<Team>();
+  private int mId;
 
 
   public Team(String name) {
     mName = name;
     instances.add(this);
+    mId = instances.size();
   }
 
   public String getTeamName() {
@@ -21,5 +23,9 @@ public class Team {
 
   public static List<Team> getAllTeams() {
     return instances;
+  }
+
+  public int getId() {
+    return mId;
   }
 }
--- src/test/java/TeamTest.java
+++ src/test/java/TeamTest.java
@@ -27,6 +27,13 @@ public class TeamTest {
   public void newTeam_addedToListOfAllTeams() {
     Team newTeam = new Team("Home");
     Team newTeam2 = new Team("Work");
-    assertEquals( newTeam, Team.getAllTeams().get(0));
+    assertEquals(newTeam, Team.getAllTeams().get(0));
+  }
+
+  @Test
+  public void newTeam_assignedIdNumberCorrectly() {
+    Team newTeam = new Team("Home");
+    Team newTeam2 = new Team("Work");
+    assertEquals(newTeam.getId(), 1);
   }
 }
