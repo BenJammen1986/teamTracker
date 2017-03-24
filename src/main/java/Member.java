7db33eea7fdb154c911b1e11cbc51bce6dc5cfed
Ben <benschenkenberger>
2017-03-24 10:46:25 -0700

    pass spec 1: team object instantiates correctly

--- src/main/java/Member.java
+++ src/main/java/Member.java
@@ -1,3 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Member {
+  private String mName;
+  private static List<Member> instances = new ArrayList<Member>();
+  private int mId;
+
+  public Member(String name) {
+    mName = name;
+    instances.add(this);
+    mId = instances.size();
+  }
+
+  public String getName() {
+    return mName;
+  }
+
+  public static void clearMemberArray() {
+    instances.clear();
+  }
+
+  public static List<Member> getAllMembers() {
+    return instances;
+  }
+
+  public int getId() {
+    return mId;
+  }
 
 }
--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -1,3 +1,15 @@
-public class Team {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Team {
+  private String mName;
+
+
+  public Team(String name) {
+    mName = name;
+  }
+
+  public String GetName() {
+    return mName;
+  }
 }
